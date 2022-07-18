3c1870b63959b23c40bfec8193125c71149ce522
Sungyeop Choo <choointern@gmail.com>
2022-07-18 12:22:05 +0900

    Time: 17 ms (33.73%), Space: 14.4 MB (12.05%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.swift
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.swift
@@ -2,11 +2,9 @@ class Solution {
     func kidsWithCandies(_ candies: [Int], _ extraCandies: Int) -> [Bool] {
         var result: [Bool] = []
         for item in candies {
-            let added = item + extraCandies
-            // let filtered = candies.filter { added < $0 }
+            // let added = item + extraCandies
 
-            // if filtered.isEmpty { result.append(true) }
-            if candies.filter { added < $0 }.isEmpty { result.append(true) }
+            if candies.filter { item + extraCandies < $0 }.isEmpty { result.append(true) }
             else { result.append(false) }
         }
 
