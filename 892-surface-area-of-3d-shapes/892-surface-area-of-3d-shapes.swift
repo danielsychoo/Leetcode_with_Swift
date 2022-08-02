13bf766e8ec77c91224f5e20daf43e2b70bf3c82
Sungyeop Choo <choointern@gmail.com>
2022-08-02 11:41:53 +0900

    Time: 93 ms (30.77%), Space: 13.9 MB (92.31%) - LeetHub

--- 892-surface-area-of-3d-shapes/892-surface-area-of-3d-shapes.swift
+++ 892-surface-area-of-3d-shapes/892-surface-area-of-3d-shapes.swift
@@ -1,15 +1,27 @@
 class Solution {
     func surfaceArea(_ grid: [[Int]]) -> Int {
-        var count: Int = 0
-
-        (0 ..< grid.count).forEach { row in
-            (0 ..< grid.count).forEach { column in
-                if grid[row][column] > 0 { count += 2 + (grid[row][column] * 4) }
-                if row > 0 { count -= min(grid[row-1][column], grid[row][column]) * 2 }
-                if column > 0 { count -= min(grid[row][column-1], grid[row][column]) * 2 }
+    var count: Int = 0
+    
+    (0 ..< grid.count).forEach { row in
+        (0 ..< grid.count).forEach { column in
+            let current = grid[row][column]
+            
+            if grid[row][column] > 0 {
+                count += 2 + (current * 4)
+            }
+            
+            if row > 0 {
+                let beforeRow = grid[row - 1][column]
+                count -= min(beforeRow, current) * 2
+            }
+            
+            if column > 0 {
+                let beforeColumn = grid[row][column-1]
+                count -= min(beforeColumn, current) * 2
             }
         }
-
-        return count
     }
+    
+    return count
+}
 }
