0303550c9234e48eae851870a21b458f40a155c0
Sungyeop Choo <choointern@gmail.com>
2022-12-27 11:38:30 +0900

    Time: 15 ms (99.68%), Space: 14.2 MB (25.32%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.swift
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.swift
@@ -11,7 +11,7 @@
 class Solution {
     
     func deleteDuplicates(_ head: ListNode?) -> ListNode? {
-        guard let head = head else { return head }
+        guard let head = head else { return nil }
         
         var node = head
         
@@ -26,45 +26,4 @@ class Solution {
         
         return head
     }
-    
-    // var deleteDuplicates = function(head) {
-    //     if(head===null) return head;
-    //     let node  = head;
-    //     while(node && node.next) {
-    //         if(node.next.val === node.val) {
-    //             node.next = node.next.next
-    //         }else {
-    //             node = node.next
-    //         }
-    //     }
-    //     return head;
-    // };
-    
-//     func recursive(
-//         _ node: ListNode?,
-//         with result: inout ListNode?
-//     ) {
-//         guard let node = node,
-//               let nextNode = node.next else { return }
-        
-//         if node.val == nextNode.val {
-            
-//             if let doubleNextNode = nextNode.next {
-//                 node.next = doubleNextNode
-                
-//                 if let result = result { result.next = node }
-//                 else { result = node }
-                
-//                 recursive(nextNode, with: &result)
-//             } else {
-//                 node.next = nil
-                
-//                 if let result = result { result.next = node }
-//                 else { result = node }
-//             }
-            
-//         } else {
-//             recursive(nextNode, with: &result)
-//         }
-//     }
 }
