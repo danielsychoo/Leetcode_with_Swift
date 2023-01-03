2416fb9d9cc101ea983fcee317a147c1279e20b6
Sungyeop Choo <choointern@gmail.com>
2023-01-03 16:14:41 +0900

    Time: 228 ms (35.64%), Space: 14.7 MB (17.02%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.swift
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.swift
@@ -2,14 +2,11 @@ class Solution {
     
     func buildArray(_ nums: [Int]) -> [Int] {
 
-        var dict: [Int: Int] = [:]
-        for i in 0...nums.count-1 {
-            dict[i] = nums[nums[i]]
-        }
+        var result = nums
 
-        var result: [Int] = []
         for i in 0...nums.count-1 {
-            result.append(dict[i]!)
+            let num = nums[nums[i]]
+            if result[i] != num { result[i] = num }
         }
 
         return result
