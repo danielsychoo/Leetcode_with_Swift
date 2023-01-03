788e4571310511ea63d79825ddac7325c53b828d
Sungyeop Choo <choointern@gmail.com>
2023-01-03 16:00:18 +0900

    Time: 104 ms (56.38%), Space: 14.6 MB (34.57%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.swift
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.swift
@@ -10,15 +10,4 @@ class Solution {
 
         return result
     }
-
-//     func buildArray(_ nums: [Int]) -> [Int] {
-
-//         var result: [Int] = []
-
-//         for i in 0...nums.count-1 {
-//             result.append(nums[nums[i]])
-//         }
-
-//         return result
-//     }
 }
