388e812211ff43d7e827b3303802f7a000e8b0e4
Sungyeop Choo <choointern@gmail.com>
2023-01-04 12:25:25 +0900

    Time: 169 ms (20.86%), Space: 14.7 MB (23.18%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.swift
+++ 1929-concatenation-of-array/1929-concatenation-of-array.swift
@@ -2,11 +2,6 @@ class Solution {
 
     func getConcatenation(_ nums: [Int]) -> [Int] {
     
-        var result = nums
-        for i in 0...nums.count-1 {
-            result.append(nums[i])
-        }
-    
-    return result
-}
+        return nums+nums
+    }
 }
