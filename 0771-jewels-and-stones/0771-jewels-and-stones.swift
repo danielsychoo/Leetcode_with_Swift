1848ff2a6bbda362e11e0cca4664146fe3921a9b
Sungyeop Choo <choointern@gmail.com>
2023-01-04 16:33:52 +0900

    Time: 4 ms (66.00%), Space: 14.4 MB (32.67%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.swift
+++ 0771-jewels-and-stones/0771-jewels-and-stones.swift
@@ -1,13 +1,27 @@
 class Solution {
-
+    
     func numJewelsInStones(_ jewels: String, _ stones: String) -> Int {
     
         var count = 0
-        jewels.forEach {
-            var jewel = $0
-            count += stones.count - stones.filter { $0 != jewel }.count
+
+        stones.forEach { stone in
+
+            jewels.forEach { jewel in
+                if stone == jewel { count += 1 }
+            }
         }
 
         return count
     }
+
+//     func numJewelsInStones(_ jewels: String, _ stones: String) -> Int {
+    
+//         var count = 0
+//         jewels.forEach {
+//             var jewel = $0
+//             count += stones.count - stones.filter { $0 != jewel }.count
+//         }
+
+//         return count
+//     }
 }
